85c4b8b4373db2e292bec5a186541463d1b75a58
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2023-05-06 21:57:59 +0300

    Changed to cross validation

--- kNN-SVM.py
+++ kNN-SVM.py
@@ -6,30 +6,27 @@ from sklearn.svm import SVC
 from sklearn.datasets import load_wine
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import make_scorer
 
 
 def train_and_test_model(dataset, model):
-    # splitting dataset - 70% for training and 30% for testing
     x, y = dataset.data, dataset.target
-    # kf = KFold(n_splits=10)
-    # for train_index, test_index in kf.split():
-    #     cross_val_score(model, x, y)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-    # training a model
-    model = model.fit(x_train, y_train)
-    # testing a model with a test set
-    predict = model.predict(x_test)
-    # Printing out a report on accuracy
-    print(f"Accuracy = {accuracy_score(y_test, predict)}")
-    print(f"Precision = {precision_score(y_test, predict, average=None)}")
-    print(f"Recall = {recall_score(y_test, predict, average=None)}")
-    print(f"F-measure = {f1_score(y_test, predict, average=None)}\n")
+    scoring = {'accuracy': make_scorer(accuracy_score),
+               'precision': make_scorer(precision_score, average='macro', zero_division=1),
+               'recall': make_scorer(recall_score, average='macro'),
+               'f1_score': make_scorer(f1_score, average='macro')}
+    scores = cross_validate(model, x, y, cv=10, scoring=scoring)
+
+    # print(f"10-Fold Validation accuracy = {scores}\n")
+    print(f"Accuracy = {scores['test_accuracy'].mean():.3f}")
+    print(f"Precision = {scores['test_precision'].mean():.3f}")
+    print(f"Recall = {scores['test_recall'].mean():.3f}")
+    print(f"F_score = {scores['test_f1_score'].mean():.3f}\n")
 
 
 # loading iris and wine dataset
@@ -40,18 +37,11 @@ wine = load_wine()
 knn_model = KNeighborsClassifier(n_neighbors=5)
 svm_model = SVC()
 
-# forming lists
-datasets = [iris, wine]
-models = [knn_model, svm_model]
-
-# for dataset in datasets:
-#     for model in models:
-#         train_and_test_model(dataset, model)
-print("iris, knn_model")
+print("iris, kNN")
 train_and_test_model(iris, knn_model)
-print("iris, svm_model")
+print("iris, SVM")
 train_and_test_model(iris, svm_model)
-print("wine, knn_model")
+print("wine, kNN")
 train_and_test_model(wine, knn_model)
-print("wine, svm_model")
+print("wine, SVM")
 train_and_test_model(wine, svm_model)
